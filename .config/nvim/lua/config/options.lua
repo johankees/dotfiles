580caf2ac2ba5aeaf71b9d4f772be7626789193a
Deavon McCaffery <dmccaffery@users.noreply.github.com>
2024-08-08 14:29:36 +0100

    chore(neovim): remove unnecessary options
    Signed-off-by: Deavon McCaffery <dmccaffery@users.noreply.github.com>

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,9 +9,6 @@ opt.autoindent = true
 -- line wrapping
 opt.whichwrap:append("<>[]hl")
 
--- appearance
-opt.background = "dark"
-
 -- backspace
 opt.backspace = "indent,eol,start"
 
@@ -19,16 +16,8 @@ opt.backspace = "indent,eol,start"
 opt.splitright = true
 opt.splitbelow = true
 
--- special keys
-opt.iskeyword:append("-")
-
 -- add a ruler
 opt.colorcolumn = "80,120"
-
--- disable providers
-for _, provider in ipairs({ "node", "python3" }) do
-  vim.g["loaded_" .. provider .. "_provider"] = 0
-end
 
 -- disable lsp logs -- this will grow infinitely so only enable it if you need it
 vim.lsp.log_level = vim.lsp.log_levels.OFF
