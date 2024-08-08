7096de4977dbc647b7565d975fa262612c4e7f24
Deavon McCaffery <dmccaffery@users.noreply.github.com>
2024-08-09 00:58:45 +0100

    chore(neovim/neo-tree): show dotfiles unless in gitignore
    Signed-off-by: Deavon McCaffery <dmccaffery@users.noreply.github.com>

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_gitignored = true,
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+}
