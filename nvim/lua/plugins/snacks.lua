e75d099393f15569aaf94be1e5f186d02dccbe5c
Deavon McCaffery <dmccaffery@users.noreply.github.com>
2025-04-04 17:50:50 +0100

    feat(nvim/lazy/snacks): show hidden files by default and swap cwd for root
    * show hidden files by default
    * hide ignored files by default
    * swap explorer and picker in cwd instead of root by default

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,5 +2,19 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = { enabled = true },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = false,
+        },
+      },
+    },
+  },
+  keys = {
+    -- swap the defaults to use cwd instead of root
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>E", "<leader>fe", desc = "Explorer Snacks (cwd)", remap = true },
+    { "<leader>e", "<leader>fE", desc = "Explorer Snacks (root dir)", remap = true },
   },
 }
